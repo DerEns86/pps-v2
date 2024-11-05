e4abe72f40cdfbdcfc5a4d81f0487f4717d8dbab
Viktor Ens <v.ens86@yahoo.de>
2024-11-05 16:24:35 +0100

    refactor tests

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../service/auth.service';
+import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const mockAuth = {
+  login: () => {},
+};
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +15,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      providers: [
+        AuthService,
+        { provide: AuthService, useValue: mockAuth },
+        provideRouter([]),
+      ],
+      imports: [LoginComponent, BrowserAnimationsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
--- src/app/pages/signup/signup.component.spec.ts
+++ src/app/pages/signup/signup.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
+import { provideRouter } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
 
+  const mockAuth = {
+    signup: () => {},
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupComponent]
-    })
-    .compileComponents();
+      imports: [SignupComponent, BrowserAnimationsModule],
+      providers: [
+        AuthService,
+        { provide: AuthService, useValue: mockAuth },
+        provideRouter([]),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
