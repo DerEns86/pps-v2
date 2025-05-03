356c10fe6d4170e57e4b3c74f3d57c4596f61f71
DerEns86 <v.ens86@yahoo.de>
2025-05-03 17:37:14 +0200

    feat: implement signup function and auth service

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {
@@ -8,31 +14,69 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../service/auth.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
   authService: AuthService = inject(AuthService);
   fb: FormBuilder = inject(FormBuilder);
 
-  signupForm: FormGroup;
+  signupForm: FormGroup = new FormGroup({});
+  authSubription!: Subscription;
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('MyComponent initialized');
     this.signupForm = this.fb.group({
       username: ['', [Validators.required]],
       email: ['', [Validators.email, Validators.required]],
       password: ['', Validators.required],
     });
   }
+
+  ngOnDestroy(): void {
+    console.log('MyComponent destroyed');
+    if (!this.authSubription.closed) {
+      console.warn('Subscription is still active');
+    }
+  }
+
   onSubmit() {
-    const rawData = this.signupForm.getRawValue();
-    console.log(rawData);
-    this.signupForm.reset();
+    if (this.signupForm.valid) {
+      const rawData = this.signupForm.getRawValue();
+      console.log(rawData);
+      this.signupForm.reset();
+      this.signupForm.markAsPristine();
+      this.signupForm.markAsUntouched();
+      this.authSubription = this.authService
+        .register(rawData.username, rawData.email, rawData.password)
+        .subscribe((res) => {
+          next: console.log('res', res);
+          error: () => {
+            console.log('error');
+          };
+          complete: () => {
+            console.log('complete');
+          };
+        });
+    }
+  }
+
+  onGoogleSignup() {
+    this.authService.registerWithGoole().subscribe();
   }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, OnDestroy } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from '@angular/fire/auth';
+import { from } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthService {
+export class AuthService implements OnDestroy {
+  private auth = inject(Auth);
 
-  constructor() { }
+  provider = new GoogleAuthProvider();
+
+  constructor() {}
+
+  ngOnDestroy(): void {}
+
+  register(username: string, email: string, password: string) {
+    const promise = createUserWithEmailAndPassword(this.auth, email, password);
+    console.log(username, email, password);
+    return from(promise);
+  }
+
+  registerWithGoole() {
+    const promise = signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        console.log('Token:', token);
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log('Google-User: ', user);
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+    return from(promise);
+  }
 }
